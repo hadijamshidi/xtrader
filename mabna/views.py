f8cb3b6f38f6221cfd7e8f968182e0df900a833f
hadijamshidi <jamshidi-mh@dena.sharif.edu>
2017-05-30 11:15:02 +0430

    fixing header and gitignores ....

--- mabna/views.py
+++ mabna/views.py
@@ -4,13 +4,14 @@ from django.http import JsonResponse, HttpResponse
 import json, requests
 from xtrader import localsetting as local
 
+
 def get(request):
-	if local.client['job'] == 'dev':
-		result = dict()
-		for k in request.GET:
-			result[k] = request.GET[k]
-		r = requests.get(local.client['url']+'/maban/api', params=result)
-	if local.client['job'] == 'server':
-		r = requests.get(local.client['url']+request.GET['url'], header=local.client['auth'])
-	print('result:\n'+r)
-	return HttpResponse(json.dumps(result))
+    if local.client['job'] == 'dev':
+        result = dict()
+        for k in request.GET:
+            result[k] = request.GET[k]
+        r = requests.get(local.client['url'] + '/maban/api', params=result)
+    if local.client['job'] == 'server':
+        r = requests.get(local.client['url'] + request.GET['url'], headers=local.client['auth'])
+    print('result:\n' + r)
+    return HttpResponse(json.dumps(result))

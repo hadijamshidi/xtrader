1e2040b685e27aace883768a0b3a90495340d8a0
hadijamshidi <jamshidi-mh@dena.sharif.edu>
2017-06-06 10:03:52 +0430

    make market watch api  ready: check last trade date and output: SymbolId

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
-from api.models import Stock,Status
+from api.models import Stock, Status
 from django.http import HttpResponse
 from api import redis
 import json
+from datetime import datetime
 
 
 # Create your views here.
@@ -9,11 +10,12 @@ def stock(request):
     stocks = Stock.objects.all()
     stocks_list = []
     for stock in stocks:
-        stocks_list.append({'id':stock.id,'symbol_id':stock.symbol_id,'mabna_id':stock.mabna_id,
-                            'mabna_name':stock.mabna_name,'mabna_english_name': stock.mabna_english_name,
-                            'mabna_short_name':stock.mabna_short_name,'mabna_kind':stock.mabna_kind})
+        stocks_list.append({'id': stock.id, 'symbol_id': stock.symbol_id, 'mabna_id': stock.mabna_id,
+                            'mabna_name': stock.mabna_name, 'mabna_english_name': stock.mabna_english_name,
+                            'mabna_short_name': stock.mabna_short_name, 'mabna_kind': stock.mabna_kind})
         print(stocks_list)
     return HttpResponse(json.dumps(stocks_list))
+
 
 def history(request):
     symbol_ids = redis.keys()
@@ -32,17 +34,20 @@ def marketwatch(request):
     if Status.objects.get(job='server').market_watch == 'ready':
         from api.models import MarketWatch
         query = request.GET['query']
-        results = MarketWatch.objects.raw('select * from api_marketwatch where {}'.format(query))
+        results = MarketWatch.objects.filter(LastTradeDate=str(datetime.today())[:10]).raw(
+            'select * from api_marketwatch where {}'.format(query))
         final_result = []
         for result in results:
             final_result.append(result.to_dict())
         return HttpResponse(json.dumps(final_result))
     else:
         return HttpResponse('database is updating please try a min later')
+
+
 def update_MarketWatch(request):
     from api import testdate
     if Status.objects.get(job='server').number_of_requests == 0:
         testdate.update_MarketWatch()
     else:
         return HttpResponse('workers are working')
-    # returnadmin.site.register(MarketWatch)
+        # returnadmin.site.register(MarketWatch)

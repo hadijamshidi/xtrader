1ff3fd3c1081f968fa41165dbeec271e0c405261
hossein <iran581@gmail.com>
2017-06-12 11:45:34 +0430

    add follower model in finace

--- finance/models.py
+++ finance/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-class strategy(models.Model):
-	description = models.Charfield(max_length=500)
-	name = models.Charfield(max_length=80)
-	filters = models.Charfield(max_length=500)
-	config = models.Charfield(max_length=500)
-	watch_list = models.Charfield(max_length=500)
-	
+class Strategy(models.Model):
+    description = models.CharField(max_length=500,null=True,blank=True)
+    name = models.CharField(max_length=80,default=' استراتژی من ')
+    filters = models.CharField(max_length=500)
+    config = models.CharField(max_length=500,null=True,blank=True)
+    watch_list = models.CharField(max_length=500)
+
+
+class Follower(models.Model):
+    owner = models.ForeignKey(User)
+    strategy = models.ForeignKey(Strategy)
+    follower = models.ForeignKey(User)

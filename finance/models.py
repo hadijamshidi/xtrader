27bc3c027cb935e55a900007d1d9219c85ffb3d7
hadijamshidi <jamshidi-mh@dena.sharif.edu>
2017-06-12 11:35:34 +0430

    create strategy model

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class strategy(models.Model):
+	description = models.Charfield(max_length=500)
+	name = models.Charfield(max_length=80)
+	filters = models.Charfield(max_length=500)
+	config = models.Charfield(max_length=500)
+	watch_list = models.Charfield(max_length=500)
+	

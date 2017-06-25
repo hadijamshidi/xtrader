275149e470f7fc7fe7bfa149041e05c931aea93e
iran581@gmail.com <hossein581>
2017-06-25 12:56:59 +0430

    comlete engine websockets

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,15 +4,17 @@ from .models import Room
 import json
 @channel_session
 def ws_connect(message):
-    # prefix, label = message['path'].strip('/').split('chat/')
-    label = 'IRO7RZIP0005'
-    data = json.loads(message['text'])
-    label = data['symbolID'][:-1]
+    # label =     label = message['path']
+    label = message['path'].strip('/').split('/')[2]
+    # label = 'IRO7RZIP0005'
+    # data = json.loads(message['text'])
+    # label = data['symbolID'][:-1]
     print(message)
     # room = Room.objects.get(label=label)
     # print(room.label)
     Group('chat-' + label).add(message.reply_channel)
     message.channel_session['room'] = label
+    hadi(label)
     ws_send(message)
     print('connected')
 
@@ -40,11 +42,7 @@ def hadi(label = 'test'):
     import time
 
     room = Room.objects.get(label=label)
-    # data = json.loads(message['text'])
-    # m = room.messages.create(handle='handle', message='yes')
-    for i in range(5):
-        m = {'handle': 'handle', 'message': 'self.message', 'timestamp': 'self.formatted_timestamp'}
+    for i in range(1):
+        m = {'handle': 'handle', 'message': 'self.message {}'.format(i), 'timestamp': 'self.formatted_timestamp'}
         Group('chat-'+label).send({'text':json.dumps(m) })
-
         time.sleep(1)
-                                   # json.dumps(m.as_dict())})

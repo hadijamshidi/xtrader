97dba00067bf5f7ecbf89a96d327c6ec1996491a
iran581@gmail.com <hossein581>
2017-07-17 18:48:08 +0430

    mongodbclear

--- finance/mongomodel.py
+++ finance/mongomodel.py
@@ -1,8 +1,8 @@
-
-from mongoengine import *
-connect(host='localhost', port=27017)
-
-class User(Document):
-    email = StringField(required=True)
-    first_name = StringField(max_length=50)
-    last_name = StringField(max_length=50)
+#
+# from mongoengine import *
+# connect(host='localhost', port=27017)
+#
+# class User(Document):
+#     email = StringField(required=True)
+#     first_name = StringField(max_length=50)
+#     last_name = StringField(max_length=50)

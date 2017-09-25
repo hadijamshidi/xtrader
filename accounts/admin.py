22785f3752ccbe24a702670c5871f6d703f9f4df
hadijamshidi <jamshidi-mh@dena.sharif.edu>
2017-09-25 12:47:28 +0330

    subscribe models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,20 +1,26 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Subscribe, Membership
 from userena.admin import UserenaAdmin
 from django.contrib.auth.models import User
 
+
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('__str__','first_name','last_name','cellPhone','email','last_login',)
+    list_display = ('__str__', 'first_name', 'last_name', 'cellPhone', 'email', 'last_login',)
+
     # list_filter = ['gender','people_type','level_type','personwith','interstToCoaoprat']
-   # ('user__first_name', 'user__last_name', 'user__username',)
+    # ('user__first_name', 'user__last_name', 'user__username',)
     def email(self, obj):
         return obj.user.email
+
     email.short_description = 'ایمیل'
 
     def make_published(modeladmin, request, queryset):
         print(queryset)
         # return queryset.order_by('-user__last_login')
-    # actions = [formfield_for_foreignkey]
+        # actions = [formfield_for_foreignkey]
+
 
 admin.site.unregister(Profile)
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Subscribe)
+admin.site.register(Membership)
--- accounts/models.py
+++ accounts/models.py
@@ -10,19 +10,30 @@ from django.utils.translation import ugettext as _
 from userena.models import UserenaBaseProfile
 
 
+class Subscribe(models.Model):
+    name = models.CharField(max_length=11, verbose_name='نام پکیج')
+    price = models.IntegerField()
+    value = models.IntegerField()
+
+
 class Profile(UserenaBaseProfile):
     user = OneToOneField(User,
                          unique=True,
                          verbose_name=_('user'),
                          related_name='my_profile')
-    # date_of_birth = DateField(blank=True, null=True)
-    # firts_name=
-    cellPhone = models.CharField(max_length=11, verbose_name='شماره تلفن ',null=True,blank=True)
-
+    cellPhone = models.CharField(max_length=11, verbose_name='شماره تلفن ', null=True, blank=True)
+    expire = models.DateField(null=True, blank=True)
 
     def last_login(self):
         return self.user.last_login
+
     def first_name(self):
         return self.user.first_name
+
     def last_name(self):
         return self.user.last_name
+
+
+class Membership(models.Model):
+    subscribe = models.ForeignKey(Subscribe)
+    profile = models.ForeignKey(Profile)

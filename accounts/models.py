4a2088abdb68949e5c64a052d8e6356642647c33
iran581@gmail.com <hossein581>
2017-10-24 16:31:19 +0330

    add vipfilter

--- accounts/models.py
+++ accounts/models.py
@@ -59,5 +59,5 @@ class Membership(models.Model):
         from sales.models import Payment
         return Payment.objects.all().filter(membership=self).first().success
 
-    def __str__(self):
-        return self.subscribe.name
+    # def __str__(self):
+    #     return self.subscribe.name
